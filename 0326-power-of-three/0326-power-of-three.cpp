c3d8aa756ff49e4bb18d0082d035a147edad8294
Anjali56-creator <anjalichaurasiya2348@gmail.com>
2025-11-17 16:19:18 +0530

    Time: 0 ms (100%), Space: 9 MB (44.04%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        while(n!=1){
+            if(n<1)
+            return false;
+            if(n%3!=0)
+            return false;
+            
+            n/=3;
+        }
+        return true;
+    }
+};
