5de81fdf28b55af7cdde35d2590ee65de8d91772
Anjali56-creator <anjalichaurasiya2348@gmail.com>
2025-11-24 07:37:54 +0530

    Time: 0 ms (100%), Space: 18.5 MB (37.78%) - LeetHub

---
+++ 1018-binary-prefix-divisible-by-5/1018-binary-prefix-divisible-by-5.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& nums) {
+      vector<bool>ans;
+      int rem=0;
+      for(int i=0;i<nums.size();i++){
+        rem=rem*2+nums[i];
+        rem=rem%5;
+        if(rem==0)
+        ans.push_back(true);
+        else
+        ans.push_back(false);
+      }
+      return ans;  
+    }
+};
