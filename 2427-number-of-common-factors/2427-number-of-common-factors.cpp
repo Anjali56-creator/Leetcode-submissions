39091635931b6c54d917ecb0214488abc5ba03f2
Anjali56-creator <anjalichaurasiya2348@gmail.com>
2025-12-28 22:01:57 +0530

    Time: 46 ms (0.48%), Space: 7.8 MB (80.13%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+        int c=0;
+        if(a==1&&b==1)
+        return 1;
+        for(int i=1;i<a*b;i++){
+            if(a%i==0&&b%i==0)
+            c++;
+        }
+        return c;
+    }
+};
