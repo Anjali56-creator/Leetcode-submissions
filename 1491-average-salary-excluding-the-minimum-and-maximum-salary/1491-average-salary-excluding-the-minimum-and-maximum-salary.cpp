e2021ddc7967e1d39b5810ba3916f2d09c042354
Anjali56-creator <anjalichaurasiya2348@gmail.com>
2026-02-10 17:19:16 +0530

    Time: 0 ms (100%), Space: 9.7 MB (10.66%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -4,7 +4,7 @@ public:
         int n=salary.size(),sum=0;
         int maxv=salary[0],minv=salary[0];
         for(int i=0;i<n;i++){
-             maxv = max(maxv,salary[i]);
+            maxv = max(maxv,salary[i]);
             minv = min(minv,salary[i]);
             sum += salary[i];
         }
