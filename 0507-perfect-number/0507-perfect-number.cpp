56241d2c884082d142ee58534b1c5b63190aa618
Anjali56-creator <anjalichaurasiya2348@gmail.com>
2026-03-02 08:47:21 +0530

    Time: 2095 ms (15.86%), Space: 7.8 MB (19.6%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int sum=0;
+        if(num<=1)
+        return false;
+        for(int i=1;i<num;i++){
+            if(num%i==0){
+                sum+=i;
+            }
+        }
+        if(sum==num)
+        return true;
+        else
+        return false;
+    }
+};
