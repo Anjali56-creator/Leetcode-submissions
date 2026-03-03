bf4458fae4f57a1e5fe4a4ba92a3a9abd1e459a3
Anjali56-creator <anjalichaurasiya2348@gmail.com>
2026-03-03 18:08:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -3,7 +3,7 @@ public:
     int xorOperation(int n, int start) {
         int c=0,a=0;
         vector<int>nums(n);
-      for(int i=0;i<nums.size();i++){
+        for(int i=0;i<nums.size();i++){
         a=start+2*i;
         c=a^c;
       }  
