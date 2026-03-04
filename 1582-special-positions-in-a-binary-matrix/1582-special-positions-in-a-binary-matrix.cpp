f74e6d1b134a2d0fe0721633297d5d11dce10dfd
Anjali56-creator <anjalichaurasiya2348@gmail.com>
2026-03-04 09:20:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -4,7 +4,6 @@ public:
         int a=0;
         for(int i=0;i<mat.size();i++){
             for(int j=0;j<mat[0].size();j++){
-               
                 int r=0,c=0;
                 for(int k=0;k<mat[0].size();k++){
                     r+=mat[i][k];
