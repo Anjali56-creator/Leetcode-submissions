b5ec8de5388a57dafa9e741fb800bb462da4ce06
Anjali56-creator <anjalichaurasiya2348@gmail.com>
2026-03-17 18:35:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-         vector<int>ans;
-         vector<bool> used(nums2.size(), false);
+        vector<int>ans;
+        vector<bool> used(nums2.size(), false);
         int n=nums1.size(),m=nums2.size();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
