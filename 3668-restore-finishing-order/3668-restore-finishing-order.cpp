17de4867c27507031d33f56b71e5ba0186d74f8d
Anjali56-creator <anjalichaurasiya2348@gmail.com>
2026-04-07 21:10:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3668-restore-finishing-order/3668-restore-finishing-order.cpp
+++ 3668-restore-finishing-order/3668-restore-finishing-order.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
         vector<int>ans;
-        for(int i=0;i<order.size();i++){
-            for(int j=0;j<friends.size();j++){
+        int n=order.size(),m=friends.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(order[i]==friends[j])
                 ans.push_back(order[i]);
             }
