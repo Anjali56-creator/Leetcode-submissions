f8d0fda10c83cb9d0c9d59baefa21abc847e8a97
Anjali56-creator <anjalichaurasiya2348@gmail.com>
2026-06-16 14:10:30 +0530

    Time: 4 ms (63.64%), Space: 39.9 MB (42.05%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string result="";
+        for(char i : s){
+            if(i=='*'){
+                if(!result.empty())
+                result.pop_back();
+            }
+            else if(i=='#'){
+            result+=result;
+            }
+            else if(i=='%'){
+            reverse(result.begin(),result.end());
+            }
+            else{
+                result.push_back(i);
+            }
+        }
+        return result;
+    }
+};
