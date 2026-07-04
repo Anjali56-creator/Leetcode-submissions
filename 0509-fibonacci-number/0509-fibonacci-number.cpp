839e07359151da29caa238d62d1f3f3a561f6edf
Anjali56-creator <anjalichaurasiya2348@gmail.com>
2026-07-04 09:26:59 +0530

    Time: 0 ms (100%), Space: 8.1 MB (16.36%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
-int DAT[1000]={0};
+
+int fibon(int n,vector<int>&dp){
+    if(n<=1)
+    return n;
+
+    if(dp[n]!=-1)
+    return dp[n];
+
+    return dp[n]=(fibon(n-1,dp)+fibon(n-2,dp));
+}
     int fib(int n) {
-       if(n<2)
-       return n;
-       else{
-        if(DAT[n]==0){
-            DAT[n]=(fib(n-1)+fib(n-2));
-        }
-        return (DAT[n]);
-       } 
+        vector<int>dp(n+1,-1);
+        return fibon (n,dp);
     }
 };
