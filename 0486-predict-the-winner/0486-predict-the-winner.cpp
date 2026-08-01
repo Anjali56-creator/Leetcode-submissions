1836b9735575f5df5f01325af8f86489f51c88d5
Anjali56-creator <anjalichaurasiya2348@gmail.com>
2026-08-01 07:51:24 +0530

    Time: 97 ms (16.11%), Space: 9.5 MB (85.12%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int winner(vector<int>& nums, int l, int r) {
-        if (l == r)
-         return nums[l];
-
-        int pickLeft  = nums[l] - winner(nums, l + 1, r);
-        int pickRight = nums[r] - winner(nums, l, r - 1);
-
+        if (l==r)
+        return nums[l];
+        int pickLeft=nums[l]-winner(nums,l+1,r);
+        int pickRight=nums[r]-winner(nums,l,r-1);
         return max(pickLeft, pickRight);
     }
-
     bool predictTheWinner(vector<int>& nums) {
-        int n = nums.size();
-        return winner(nums, 0, n - 1) >= 0;
+        int n=nums.size();
+        return winner(nums,0,n-1)>=0;
     }
 };
